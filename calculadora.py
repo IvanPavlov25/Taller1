1657e2f3497650fd7c323b1e4873587bc8e411b5
IvanPavlov <100708591+IvanPavlov25@users.noreply.github.com>
2023-08-12 11:39:36 -0500

    tercera calculadora, suma, resta y multiplica

--- calculadora.py
+++ calculadora.py
@@ -4,11 +4,15 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+def multiply(x, y):
+    return x * y
+
 print("Seleccione la operacion:")
 print("1. Sumar")
 print("2. Restar")
+print("3. Multiplicar")
 
-choice = input("Haga su elección (1/2): ")
+choice = input("Haga su elección (1/2/3): ")
 
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
@@ -17,5 +21,7 @@ if choice == '1':
     print(num1, "+", num2, "=", add(num1, num2))
 elif choice == '2':
     print(num1, "-", num2, "=", subtract(num1, num2))
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
 else:
     print("Entrada inválida")
